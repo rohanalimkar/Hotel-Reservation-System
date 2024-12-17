533e7042ea272d1edb4f2d9d2f07f0a5df6ab01b
Rohan <rohanalimkar@gmail.com>
2024-12-17 15:44:42 +0530

    created customer login

--- src/main/java/Client_Application_Main/HotelReservationSystem.java
+++ src/main/java/Client_Application_Main/HotelReservationSystem.java
@@ -9,20 +9,41 @@ public class HotelReservationSystem {
 		try {
 			do {
 			System.out.println("******Welcom To Hotel Reservation System*****");
-			System.out.println("1.Customer.\n2.Admin.\n3.Close System.");
+			System.out.println("Login in Application\n1.Customer.\n2.Admin.\n3.Close System.");
 			System.out.println("Enter Your Choice");
 			int login=sc.nextInt();
 			switch(login)
 			{
 			case 1:{
-				
-					}	
-					break;
+					do {
+					System.out.println("1.Already have Customer Account\n2.Create New Account.\n3.Close");
+					System.out.println("Enter Your Choice");
+					int Ac=sc.nextInt();
+					switch(Ac)
+					{
+					case 1:
+					{
+						
+					}
+							break;
+					case 2:
+					{
+						
+					}
+						break;
+					case 3:
+						System.out.println("Thank You for visiting");
+						System.exit(0);
+						default:System.out.println("You enter wrong choice.");
+					}
+					}while(true);	
+			}
 			case 2:{
 				
 				}
 				break;
 			case 3:
+				System.out.println("Thank You for visiting");
 				System.exit(0);
 			default:System.out.println("You Enetr Wrong Choice.");
 			}
