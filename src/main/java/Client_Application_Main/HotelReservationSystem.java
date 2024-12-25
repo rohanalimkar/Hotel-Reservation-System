d83b19f9aca99e61de6ebe054b1047764e5a9c79
rohanalimkar <rohanalimkar@gmail.com>
2024-12-25 10:00:00 +0530

    changes in main function
    changes in main function pushed by Rohan

--- src/main/java/Client_Application_Main/HotelReservationSystem.java
+++ src/main/java/Client_Application_Main/HotelReservationSystem.java
@@ -43,7 +43,7 @@ import Services.StateService;
 import Services.StateServiceImp;
 import model.StateModel;
 
-public class HotelReservationSystem {
+public class HotelReservationSystem {  //main class
 
 		public static Logger log = Logger.getLogger(HotelReservationSystem.class);
 		static
